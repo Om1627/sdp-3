90412e72ec74a9b7919d52175991e091f57aceb4
Om1627 <60969676+Om1627@users.noreply.github.com>
2020-06-04 17:09:28 +0530

    Update Form.js

--- Classes/Form.js
+++ Classes/Form.js
@@ -33,13 +33,13 @@ class Form{
             camera.position.y=camera.position.y+1
   
 
-         var heading=createElement('Morning of 21st Jan,1930')  
+         var heading=createDiv('Morning of 21st Jan,1930')  
                              
          heading.position(displayWidth/2-500,300)
          heading.style('color',"white")
          heading.style('font-size', '70px')
 
-         var StoryTillNow=createElement('Me and my friend took off for hiking on this mountain 5 days ago. We lost our way and we could not find our way back. There are clouds everywhere.Things got worse yesterday, when my friend disappeared in a dark cave. He started screaming, but I could not risk my life to save his. I tried to find someone for help, and today I have come across this little, neat house. I hope someone lives here....')  
+         var StoryTillNow=createDiv('Me and my friend took off for hiking on this mountain 5 days ago. We lost our way and we could not find our way back. There are clouds everywhere.Things got worse yesterday, when my friend disappeared in a dark cave. He started screaming, but I could not risk my life to save his. I tried to find someone for help, and today I have come across this little, neat house. I hope someone lives here....')  
                              
          StoryTillNow.position(displayWidth/2-600,500)
          StoryTillNow.style('color',"white")
